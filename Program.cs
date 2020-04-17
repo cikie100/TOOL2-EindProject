b213b6be7bb7ad248cc1e90a5d5b096500167d2d
Crow Scare <ciki3100@gmail.com>
2020-04-17 17:49:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tool2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
